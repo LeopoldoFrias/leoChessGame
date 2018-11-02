dc08e81a15d7c29e55d04b71448fd3ad25ee263b
LeopoldoFrias <44326230+LeopoldoFrias@users.noreply.github.com>
2018-11-01 17:30:57 -0700

    Add files via upload

--- backup.py
+++ backup.py
@@ -1,29 +1,4 @@
-import sys, pygame, itertools
-
-from pygame.locals import MOUSEBUTTONDOWN, MOUSEBUTTONUP
-from MoreOfChess import Board as Board
-
-pygame.init()
-
-HEIGHT = 800
-WIDTH = 1000
-BLACK = pygame.Color('black')
-WHITE = pygame.Color('white')
-
-# This is where the chess board is actually being made
-colors = itertools.cycle((WHITE, BLACK))
-tile_size = 90
-width, height = 8*tile_size, 8*tile_size
-background = pygame.Surface((width, height))
-
-for y in range(0, height, tile_size):
-    for x in range(0, width, tile_size):
-        rect = (x, y, tile_size, tile_size)
-        pygame.draw.rect(background, next(colors), rect)
-    next(colors)
-
-
-    def printBoard(self):
+def printBoard(self):
         count = 0
         for tiles in range(64):
             print('|', end=self.boardTiles[tiles].pieceOnTile.toString())
@@ -137,7 +112,6 @@ class NullPiece():
     def __init__(self):
         pass
 
-
 class Tile:
 
     def __init__(self, pos):
@@ -145,104 +119,38 @@ class Tile:
         self.pieceOnTile = piece
 
 
-# Sprites = pygame.Surface((100, 100))
+class Queen(pygame.sprite.Sprite):
 
-# simple variables
-pygame.display.set_caption("TXT Chess")
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-clock = pygame.time.Clock()
-
-piece = []
-
-
-# ------------------------------------------------------------------------------------------------------------------
-# What conditions will be if their is not piece present at that time
-
-
-
-class King(pygame.sprite.Sprite):
-
-    def __init__(self,pos):
-
-        super().__init__()
-
-        self.image = pygame.image.load("./images/Chess_tile_kd.png")
-        self.rect = pygame.Rect(pos[0], pos[0], 50, 50)
+    def __init__():
+        super().__init__(self, pos)
+        self.image = pygame.image.load('./images/Chess_tile_qd.png').convert()
+        self.rect = self.image.get_rect()
 
 class Bishop(pygame.sprite.Sprite):
 
-    def __init__(self,pos):
+    def __init__(self, pos):
+        super().__init__(self)
+        self.image = pygame.image.load('.images/Chess_tile_bd.png').convert()
+        self.rect = self.image.get_rect()
 
-        super().__init__()
-
-        self.image = pygame.image.load("./images/Chess_tile_bd.png")
-        self.rect = pygame.Rect(pos[0], pos[0], 50, 50)
-
-class Knight(pygame.sprite.Sprite):
-
-    def __init__(self,pos):
-
-        super().__init__()
-
-        self.image = pygame.image.load("./images/Chess_tile_nd.png")
-        self.rect = pygame.Rect(pos[0], pos[0], 50, 50)
-
-class Pawn(pygame.sprite.Sprite):
-
-    def __init__(self,pos):
-
-        super().__init__()
-
-        self.image = pygame.image.load("./images/Chess_tile_pd.png")
-        self.rect = pygame.Rect(pos[0], pos[0], 50, 50)
 
 class Castle(pygame.sprite.Sprite):
 
-    def __init__(self,pos):
+    def __init__(self, pos):
+        super().__init__(self)
+        self.image = pygame.image.load('.images/Chess_tile_rd.png').convert()
+        self.rect = self.image.get_rect()
 
-        super().__init__()
+class Knight(pygame.sprite.Sprite):
 
-        self.image = pygame.image.load("./images/Chess_tile_rd.png")
-        self.rect = pygame.Rect(pos[0], pos[0], 50, 50)
+    def __init__(self, pos):
+        super().__init__(self)
+        self.image = pygame.image.load('.images/Chess_tile_nd.png').convert()
+        self.rect = self.image.get_rect()
 
-class Queen(pygame.sprite.Sprite):
+class Pawn(pygame.sprite.Sprite):
 
-    def __init__(self,pos):
-
-        super().__init__()
-
-        self.image = pygame.image.load("./images/Chess_tile_qd.png")
-        self.rect = pygame.Rect(pos[0], pos[0], 50, 50)
-
-# ------------------------------------------------------------------------------------------------------------
-# Create pieces
-
-# white pieces
-white_team = pygame.sprite.Group()
-
-king = King([0, 0])
-queen = Queen([100,100])
-castle = Castle([200, 200])
-knight = Knight([180, 180])
-pawn = Pawn([170, 170])
-bishop = Bishop([400, 130])
-white_team.add(king, queen, castle, knight, pawn, bishop)
-
-# Constantly active variables during run time
-while 1:
-    for event in pygame.event.get():
-       if event.type == pygame.QUIT:
-           sys.exit()
-
-
-# The screen display is being "drawn" (updated) to show board and pieces
-    screen.fill((60, 70, 90))
-    screen.blit(background, (100, 100))
-
-    white_team.draw(screen)
-    pygame.display.update()
-    pygame.display.update()
-    clock.tick(60)
-    pygame.display.flip()
-
-pygame.quit()
+    def __init__(self, pos):
+        super().__init__(self)
+        self.image = pygame.image.load('./images/Chess_tile_pd.png').convert()
+        self.rect = self.image.get_rect()
